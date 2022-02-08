9f33ab7ddb5f9bad823bb46d554b022595fb39df
Weston Slaughter <westonslaughter@gmail.com>
2022-02-08 13:14:37 -0500

    "ratio scatter & regressuin, Ca:Si,Mg"

--- analysis.R
+++ analysis.R
@@ -13,178 +13,39 @@ library(stringr)
 
 source("project_helpers.R")
 
-theme_set(ggthemes::theme_few())
-
-
-#### Helper Function ####
-#load authorization file for macrosheds google sheets
-## googlesheets4::gs4_auth(path = '../../data_processing/googlesheet_service_accnt.json')
-## sm <- suppressMessages
-
-## #read in secrets
-## conf <- jsonlite::fromJSON(readLines('src/config.json'),
-##                            simplifyDataFrame = FALSE)
-
-## load_config_datasets(from_where = 'remote')
-
-## site_data <- filter(site_data,
-##                     as.logical(in_workflow))
-
-## network_domain <- site_data %>%
-##     select(network, domain) %>%
-##     distinct() %>%
-##     arrange(network, domain)
-
-## dom_net <- site_data %>%
-##   select(network, domain, site_code)
-
 ## Watershed Attribute Groupings
 ws_summaries <- read.csv("src/spatial/watershed_summaries.csv")
 
 #### Data read and munging ####
-
 var_interest <- c('NO3_N', 'NO3_NO2_N', 'Ca', 'Cl', 'Mg', 'discharge', 'Na', 'pH', 'SiO2_Si', 'SiO3_Si', 'Si', "SiO2", 'SiO3', 'K', 'Mg')
-all_chem <- tibble()
+chem_fils <- list.files("src/macrosheds_dataset_v1/lter/hbef/derived/stream_chemistry__ms006/", full.names = TRUE)
+q_fils <- list.files("src/macrosheds_dataset_v1/lter/hbef/derived/discharge__ms003/", full.names = TRUE)
 
-## for(i in 1:nrow(network_domain)){
-
-##     network <- network_domain[[i,1]]
-##     domain <- network_domain[[i,2]]
-
-##     stream_gauges <- site_data %>%
-##         filter(site_type == 'stream_gauge',
-##                network == !!network,
-##                domain == !!domain,
-##                in_workflow == 1) %>%
-##         pull(site_code)
-
-##     derive_files <- list.files(glue('~/files/projects/science/macrosheds/nitrate/nitrate_patterns/src/macrosheds_dataset_v1/{n}/{d}/derived',
-##                                     n = network,
-##                                     d = domain),
-##                                full.names = TRUE)
-
-##     stream_chemistry <- grep('stream_chemistry', derive_files, value = TRUE)
-
-##     if(!length(stream_chemistry) == 1){
-##         stop(glue('{n} {d} is has more than one or is missing a stream chemistry file',
-##                   n = network, d = domain))
-##     }
-
-##     all_chem_fils <- list.files(stream_chemistry, full.names = TRUE)
-
-##     site_chem <- map_dfr(all_chem_fils, read_feather) %>%
-##         # Filter for stream Guages
-##         filter(site_code %in% !!stream_gauges)
-
-##     site_chem <- site_chem %>%
-##         mutate(var = macrosheds::ms_drop_var_prefix(var)) %>%
-##         filter(var == !!var_interest)
-
-##     ## if((!'NO3_N' %in% macrosheds::ms_drop_var_prefix(unique(site_chem$var))) &&
-##     ##    'NO3_NO2_N' %in% macrosheds::ms_drop_var_prefix(unique(site_chem$var))){
-
-##     ##     site_chem <- site_chem %>%
-##     ##         mutate(var = macrosheds::ms_drop_var_prefix(var)) %>%
-##     ##         filter(var == 'NO3_NO2_N')
-##     ## } else{
-##     ##     site_chem <- site_chem %>%
-##     ##         mutate(var = macrosheds::ms_drop_var_prefix(var)) %>%
-##     ##         filter(var == !!var_interest)
-##     ## }
-
-##     all_chem <- rbind(all_chem, site_chem)
-## }
-
-## all_fils <- list.files("src/macrosheds_dataset_v1/lter/hbef/derived/stream_chemistry__ms006/", full.names = TRUE)
-## d <- map_dfr(all_fils, read_feather)%>%
-##     filter(ms_status == 0,
-##            ms_interp == 0) %>%
-##     select(-ms_status, -ms_interp, -val_err)  %>%
-##     pivot_wider(names_from = 'var', values_from = 'val')
-
-## n <- all_chem %>%
-##     mutate(year = year(datetime),
-##            month = month(datetime),
-##            val = as.numeric(val)) %>%
-##     #dplyr::filter(val > 0) %>%
-##     dplyr::filter(ms_interp == 0,
-##                   ms_status == 0) %>%
-##     dplyr::filter(domain != 'neon') %>%
-##     select(-val_err)
-
-## var_intrest <- 'discharge'
-## all_q <- tibble()
-
-## for(i in 1:nrow(network_domain)){
-
-##     network <- network_domain[[i,1]]
-##     domain <- network_domain[[i,2]]
-
-##     stream_gauges <- site_data %>%
-##         filter(site_type == 'stream_gauge',
-##                network == !!network,
-##                domain == !!domain,
-##                in_workflow == 1) %>%
-##         pull(site_code)
-
-##     derive_files <- list.files(glue('~/files/projects/science/macrosheds/nitrate/nitrate_patterns/src/macrosheds_dataset_v1/{n}/{d}/derived',
-##                                     n = network,
-##                                     d = domain),
-##                                full.names = TRUE)
-
-##     discharge <- grep('discharge', derive_files, value = TRUE)
-
-##     if(!length(discharge) == 1){
-##         stop(glue('{n} {d} is has more than one or is missing a stream chemistry file',
-##                   n = network, d = domain))
-##     }
-
-##     all_q_fils <- list.files(discharge, full.names = TRUE)
-
-##     site_q <- map_dfr(all_q_fils, read_feather) %>%
-##         # Filter for stream Guages
-##         filter(site_code %in% !!stream_gauges) %>%
-##         # Filter varible
-##         mutate(var = macrosheds::ms_drop_var_prefix(var)) %>%
-##         filter(var == !!var_intrest)
-
-##     if('year' %in% names(site_q)){
-##         site_q <- site_q %>%
-##             select(-year)
-##     }
-
-##     all_q <- rbind(all_q, site_q)
-## }
-
-## all_q <- all_q %>%
-##     mutate(year = year(datetime),
-##            month = month(datetime),
-##            val = as.numeric(val)) %>%
-##     select(-var, q = val, -ms_status, -ms_interp, -val_err)
-
-## qc <- inner_join(n, all_q, by = c()) ## %>%
-##     ## filter(!is.na(val))
-
-## write.csv(qc, 'chem.csv')
-
-## hubbard <- qc %>% filter(grepl("^w[0-9]", site_code))
-## write.csv(hubbard, 'hubbard_chem.csv')
-
-## casi <- hubbard %>% filter(grepl("Ca|Si", var))
-## wide_casi <- casi %>% pivot_wider(names_from = var, values_from = val)
-
-## write.csv(wide_casi, 'casi.csv')
-
-
-## Holy Grail
-all_fils <- list.files("src/macrosheds_dataset_v1/lter/hbef/derived/stream_chemistry__ms006/", full.names = TRUE)
-
-d <- map_dfr(all_fils, read_feather)%>%
+hbef_chem <- map_dfr(chem_fils, read_feather)%>%
     filter(ms_status == 0,
            ms_interp == 0) %>%
     select(-ms_status, -ms_interp, -val_err)  %>%
-        mutate(var = macrosheds::ms_drop_var_prefix(var)) %>%
+    mutate(var = macrosheds::ms_drop_var_prefix(var),
+         year = year(datetime),
+         month = month(datetime),
+         val = as.numeric(val)) %>%
         ## filter(var == !!var_intrest) %>%
   pivot_wider(names_from = 'var', values_from = 'val')
 
-hubbard <- d[c("datetime", "site_code", "Ca", "SiO2_Si", "M", "Cl", "")]
+
+hbef_q <- map_dfr(q_fils, read_feather) %>%
+  mutate(var = macrosheds::ms_drop_var_prefix(var),
+         year = year(datetime),
+         month = month(datetime),
+         val = as.numeric(val)) %>%
+  filter(ms_status == 0,
+         ms_interp == 0) %>%
+  select(-ms_status, -ms_interp, -val_err) %>%
+  pivot_wider(names_from = 'var', values_from = 'val')
+
+hbef <- merge(hbef_chem, hbef_q, by = c("datetime", "year", "month", "site_code"))
+hbef_elements <- hbef[c("datetime", "year", "month", "site_code", "discharge",
+                     "Ca", "SiO2_Si", "Mg", "Cl", "Na", "K")]
+
+write.csv(hbef, "hbef.csv")
+write.csv(hbef_elements, "hbef_elements.csv")
--- analysis.py
+++ analysis.py
@@ -3,14 +3,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import statsmodels.api as sm
+from numpy.polynomial.polynomial import polyfit
+from scipy import stats
 import datetime as dt
 
-hubbard = pd.read_csv('hubbard_chem.csv', index_col=0)
+hubbard = pd.read_csv('hbef_elements.csv', index_col=0)
 
-hubbard_wide = pd.pivot_table(hubbard,
-                              index=['datetime', 'year', 'month', 'site_code', 'q', 'ms_status', 'ms_interp'],
-                              values='val',
-                              columns=['var'])
+# hubbard_wide = pd.pivot_table(hubbard,
+#                               index=['datetime', 'year', 'month', 'site_code', 'q', 'ms_status', 'ms_interp'],
+#                               values='val',
+#                               columns=['var'])
 
 experimental_history = {
     'w1': 'ca_addition',
@@ -25,24 +28,190 @@ experimental_history = {
 }
 
 colors_history = {
+    'timber_operation': 'red',
     'ca_addition': 'orange',
     'reference': 'blue',
-    'timber_operation': 'red',
 }
 
-# Hubbard Element Ratios
-# Ca:Si
-ca_si = hubbard_wide[['Ca', 'SiO2_Si']].reset_index()
-ca_si_nona = ca_si.dropna()
+# helper functions
+def is_outlier(points, thresh=3.5):
+    """
+    Returns a boolean array with True if points are outliers and False
+    otherwise.
 
-# ca_si_nona = ca_si_nona[ca_si_nona['site_code'].str.match("w[0-9]")]
+    Parameters:
+    -----------
+        points : An numobservations by numdimensions array of observations
+        thresh : The modified z-score to use as a threshold. Observations with
+            a modified z-score (based on the median absolute deviation) greater
+            than this value will be classified as outliers.
 
+    Returns:
+    --------
+        mask : A numobservations-length boolean array.
+
+    References:
+    ----------
+        Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+        Handle Outliers", The ASQC Basic References in Quality Control:
+        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
+    """
+    if len(points.shape) == 1:
+        points = points[:,None]
+    median = np.median(points, axis=0)
+    diff = np.sum((points - median)**2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+
+##### Hubbard Element Ratios Dataset Prep
+hbef_elements = hubbard[['datetime', 'year', 'month', 'site_code', 'discharge',
+                         'Ca', 'SiO2_Si', 'Mg', 'Na', 'Cl', 'K']]
+hbef_elements['log_q'] = np.log(hbef_elements['discharge'])
+
+# appending site experimental history as categorical variable
 history  = []
-
-for site in ca_si_nona['site_code']:
+for site in hbef_elements['site_code']:
     history.append(experimental_history[site])
+hbef_elements['history'] = history
 
-ca_si_nona['history'] = history
+# ratio dfs
+casi = hbef_elements[['datetime', 'year', 'month', 'site_code', 'discharge', 'log_q', 'history',
+                      'Ca', 'SiO2_Si']].dropna()
+camg = hbef_elements[['datetime', 'year', 'month', 'site_code', 'discharge', 'log_q', 'history',
+                      'Ca', 'Mg']].dropna()
+cana = hbef_elements[['datetime', 'year', 'month', 'site_code', 'discharge', 'log_q', 'history',
+                      'Ca', 'Na']].dropna()
+# Ca:Si
+casi['casi_ratio'] = (casi['Ca'] / casi['SiO2_Si'])
+# Ca:Mg
+camg['camg_ratio'] = (camg['Ca'] / camg['Mg'])
+# Ca:Na
+cana['cana_ratio'] = (cana['Ca'] / cana['Na'])
+
+# Ca by SiO2_Si by History
+fig, ax = plt.subplots()
+
+ax.set_xlabel('SiO2_Si', fontsize=16)
+ax.set_ylabel('Ca', fontsize=16)
+ax.set_title('Ca by SiO2_Si in Hubbard Brook', fontsize=16)
+
+for history in colors_history.keys():
+    this = casi[casi['history'] == history]
+    color = colors_history[history]
+    ax.scatter(this['SiO2_Si'], this['Ca'], c=color, label=history, alpha=0.5)
+
+ax.legend(title='Experimental History')
+
+##### Changes in Element Ratios with Flow
+#### Calcium and Silica
+## Ca:SiO2_Si by Q
+fig, ax = plt.subplots()
+
+ax.set_xlabel('Discharge (Q) L/s', fontsize=16)
+ax.set_ylabel('Ca:SiO2_Si', fontsize=16)
+ax.set_title('Ca:SiO2_Si Ratio by Discharge', fontsize=16)
+
+for history in colors_history.keys():
+    this = casi[casi['history'] == history]
+    color = colors_history[history]
+    ax.scatter(this['discharge'], this['casi_ratio'], c=color, label=history, alpha=0.5)
+
+ax.legend(title="Experimental History")
+
+## Ca:SiO2_Si by log(Q)
+# remove outliers from casi_ratio values
+casi_z = casi[~is_outlier(casi['casi_ratio'])]
+
+# reorder colors for better image
+colors_history = {
+    'timber_operation': 'red',
+    'reference': 'blue',
+    'ca_addition': 'orange',
+}
+
+fig, ax = plt.subplots()
+
+ax.set_xlabel('log(Q) L/s', fontsize=16)
+ax.set_ylabel('Ca:SiO2_Si', fontsize=16)
+plt.suptitle('Ca:SiO2_Silica Casi_Ratio by log(Q)', fontsize=16)
+plt.title('points with Z score greater than 3.5 removed', fontsize=10, y=1)
+
+
+for history in colors_history.keys():
+    this = casi_z[casi_z['history'] == history]
+    color = colors_history[history]
+    ax.scatter(this['log_q'], this['casi_ratio'], c=color, label=history, alpha=0.5)
+
+ax.legend(title="Experimental History")
+
+#### Calcium and Magnesium
+## Ca:Mg by Q
+fig, ax = plt.subplots()
+
+ax.set_xlabel('Discharge (Q) L/s', fontsize=16)
+ax.set_ylabel('Ca:Mg', fontsize=16)
+ax.set_title('Ca:Mg Ratio by Discharge', fontsize=16)
+
+for history in colors_history.keys():
+    this = camg[camg['history'] == history]
+    color = colors_history[history]
+    ax.scatter(this['discharge'], this['camg_ratio'], c=color, label=history, alpha=0.5)
+
+ax.legend(title="Experimental History")
+
+## Ca:Mg Ratio by log(Q)
+# remove outliers from camg_ratio values
+camg_z = camg[~is_outlier(camg['camg_ratio'], thresh=3)]
+# or don't
+# camg_z = camg
+
+# reorder colors for better image
+colors_history = {
+    'reference': 'blue',
+    'timber_operation': 'red',
+    'ca_addition': 'orange',
+}
+
+fig, ax = plt.subplots()
+
+ax.set_xlabel('log(Q) L/s', fontsize=16)
+ax.set_ylabel('Ca:Mg', fontsize=16)
+plt.suptitle('Ca:Mg Ratio by log(Q)', fontsize=16)
+plt.title('points with Z score greater than 3 removed', fontsize=10)
+
+# removing inf values (apparently there are 12?)
+camg = camg[camg.replace([np.inf, -np.inf], np.nan).notnull().all(axis=1)]
+camg_z = camg_z[camg_z.replace([np.inf, -np.inf], np.nan).notnull().all(axis=1)]
+
+# adding regression lines
+for history in colors_history.keys():
+    this = camg_z[camg_z['history'] == history]
+    x = this['log_q']
+    y = this['camg_ratio']
+
+    color = colors_history[history]
+    ax.scatter(x, y, c=color, label=history, alpha=0.25)
+
+    m, b = np.polyfit(x, y, 1)
+    X_plot = np.linspace(ax.get_xlim()[0], ax.get_xlim()[1],100)
+    ax.plot(X_plot, m*X_plot + b, '-', color=color)
+
+ax.legend(title="Experimental History")
+
+# Ca:Na
+# by Q
+
+
+
+
+
+
+
+
 
 # date
 # x = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in ca_si['datetime']]
@@ -52,41 +221,16 @@ ca_si_nona['history'] = history
 # plt.plot(x, y)
 # plt.gcf().autofmt_xdate()
 
-# ratio
-ca_si_nona['ratio'] = (ca_si_nona['Ca'] / ca_si_nona['SiO2_Si'])
-ca_si_nona['log_q'] = np.log(ca_si_nona['q'])
+# regression
+x = np.array(camg['log_q'].values)
+y = np.array(camg['camg_ratio'].values)
 
-# Ca by SiO2_Si
-x = ca_si_nona['SiO2_Si']
-y = ca_si_nona['Ca']
-plt.scatter(x, y, c=ca_si_nona['history'].map(colors_history) , alpha=0.5)
+gradient, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+mn=np.min(x)
+mx=np.max(x)
+x1=np.linspace(mn,mx,500)
+y1=gradient*x1+intercept
 
-
-# Ca:SiO2_Si by Q
-fig, ax = plt.subplots()
-ax.plot([1, 2])
-
-ax.set_xlabel('Discharge (Q) L/s', fontsize=16)
-ax.set_ylabel('Ca:SiO2_Si', fontsize=16)
-ax.set_title('Ca:SiO2_Silica Ratio by Discharge', fontsize=16)
-
-x = ca_si_nona['q']
-y = ca_si_nona['ratio']
-plt.scatter(x, y, alpha=0.5)
-
-# log Q
-fig, ax = plt.subplots()
-ax.plot([1, 2])
-
-ax.set_xlabel('log(Q) L/s', fontsize=16)
-ax.set_ylabel('Ca:SiO2_Si', fontsize=16)
-ax.set_title('Ca:SiO2_Silica Ratio by log(Discharge)', fontsize=16)
-
-x = ca_si_nona['log_q']
-plt.scatter(x, y, alpha=0.5)
-
-# Ca:Mg
-# by Q
-
-# Ca:Na
-# by Q
+plt.plot(x,y,'ob')
+plt.plot(x1,y1,'-r')
+plt.show()
